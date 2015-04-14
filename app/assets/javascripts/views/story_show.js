eff013baab6fede291c92011effb3d19ee9a262b
Haley <haleymthurston@gmail.com>
2015-04-14 00:22:26 -0700

    new story preview on responses

--- app/assets/javascripts/views/story_show.js
+++ app/assets/javascripts/views/story_show.js
@@ -22,13 +22,16 @@ Large.Views.StoryShow = Backbone.View.extend({
       data: { current_user: true }
     });
     var newStory = new Large.Models.Story({ story_id: this.story.id });
-    var newStoryView = new Large.Views.NewStory({ collection: this.stories, model: newStory, publications: Large.Collections.publications });
+    var newStoryView = new Large.Views.NewStoryPreview({ collection: this.stories, model: newStory, publications: Large.Collections.publications });
 
     this.$('.post-click').prepend(newStoryView.render().$el);
-
+    var editor = new MediumEditor('.editable', {
+      placeholder: "",
+      buttons: ['bold', 'italic', 'quote', 'anchor']
+    });
     return this;
   },
-  
+
   showNewStory: function () {
     this.$('.post-click').css('display', 'block');
     this.$('.pre-click').css('display', 'none');
